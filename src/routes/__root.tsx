7fd912779e269e66c4b9ba5e956fbfe56f73ace2
augusto <augusto.k.ogawa@gmail.com>
2026-07-13 21:00:10 -0300

    chore: alterar logo

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -95,6 +95,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         href: appCss,
       },
       { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "apple-touch-icon", href: "/apple-touch-icon.png" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
